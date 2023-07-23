7632db65313af244e258c9a822d00a35c737543b
MachadiDev <machadianhanguera@gmail.com>
2023-07-23 00:39:49 -0300

    Criação do projeto e navegação lateral

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenuLateralComponent } from './components/menu-lateral/menu-lateral.component';
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatIconModule} from '@angular/material/icon';
+import { SuasBibliotecasComponent } from './components/suas-bibliotecas/suas-bibliotecas.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuLateralComponent,
+    HomePageComponent,
+    SuasBibliotecasComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
