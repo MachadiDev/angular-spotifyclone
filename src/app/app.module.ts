48f12e446e3f1c4092fb8f788e0b1906d2e27d3b
MachadiDev <machadianhanguera@gmail.com>
2023-08-06 23:40:10 -0300

    spotify clone app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,13 +8,21 @@ import { HomePageComponent } from './pages/home-page/home-page.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatIconModule} from '@angular/material/icon';
 import { SuasBibliotecasComponent } from './components/suas-bibliotecas/suas-bibliotecas.component';
+import { SmallCardComponent } from './components/cards/small-card/small-card.component';
+import { PrincipalContentComponent } from './components/principal-content/principal-content.component';
+import { MediumCardComponent } from './cards/medium-card/medium-card.component';
+import { MediumCardsComponent } from './components/cards/medium-cards/medium-cards.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MenuLateralComponent,
     HomePageComponent,
-    SuasBibliotecasComponent
+    SuasBibliotecasComponent,
+    SmallCardComponent,
+    PrincipalContentComponent,
+    MediumCardComponent,
+    MediumCardsComponent
   ],
   imports: [
     BrowserModule,
